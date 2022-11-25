f992635338cf7d744d76f338f57846054f3a0a3a
Deep Soni <deep.infynno@gmail.com>
2022-11-25 12:06:59 +0530

    chore: project structure setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from "react";
+import { NextPage } from "next";
+import type { AppProps } from "next/app";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import "@styles/globals.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
