cf574c0b98efdf90f06024bc7e691351312f08ec
Deep Soni <deep.infynno@gmail.com>
2022-12-13 11:34:49 +0530

    fix:header changes

--- components/layouts/Navigation.tsx
+++ components/layouts/Navigation.tsx
@@ -92,6 +92,29 @@ const Navigation = () => {
                   <NavLink key={i} href={item.href} title={item.title} />
                 ))}
             </div>
+            <button
+              data-collapse-toggle="navbar-hamburger"
+              type="button"
+              className="inline-flex items-center text-sm text-gray-500 rounded-lg "
+              aria-controls="navbar-hamburger"
+              aria-expanded="false"
+              onClick={() => setShowSidebar(!showSidebar)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
             <div className="block lg:hidden">
               {showSidebar ? (
                 <IoMdClose
@@ -99,29 +122,7 @@ const Navigation = () => {
                   onClick={() => setShowSidebar(!showSidebar)}
                 />
               ) : (
-                <button
-                  data-collapse-toggle="navbar-hamburger"
-                  type="button"
-                  className="inline-flex items-center text-sm text-gray-500 rounded-lg "
-                  aria-controls="navbar-hamburger"
-                  aria-expanded="false"
-                  onClick={() => setShowSidebar(!showSidebar)}
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <svg
-                    className="w-6 h-6"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
+                ""
               )}
 
               <div
